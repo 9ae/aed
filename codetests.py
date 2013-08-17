67f73c2965ac472a66e9b12c2e97c41dabe9eed8
Alice <alice@valour.me>
2013-08-17 15:50:46 -0400

    made time helper class

--- codetests.py
+++ codetests.py
@@ -1,10 +1,10 @@
 import sysaid as sa
 
 def timed_loop_test():
-	a = sa.time_now()
-	for i in range(0,1999):
-		print i
-	b = sa.time_now()
-	print "loop runtime=%f"%(b-a)
+  T = sa.Toki()
+  for i in range(0,1999):
+    print i
+  b = T.diff(-6)
+  print "loop runtime=%f %s"%(b[0],b[1])
 
 timed_loop_test()
--- sysaid.py
+++ sysaid.py
@@ -14,6 +14,28 @@ def time_now():
 	import time
 	my_os = find_os()
 	if my_os==0:
-		return time.clock()
+                # gets time in seconds as float
+		return time.clock()	
 	elif my_os==1 or my_os==2:
+		#gets time in seconds
 		return time.time()
+
+def dt_now():
+        from datetime import datetime
+        dt = datetime.now()
+        return dt.microsecond
+
+from datetime import datetime
+class Toki:
+
+  def __init__(self):
+    self.startTime = datetime.now()
+
+  def diff(self, unit=1):
+    time_diff = datetime.now() - self.startTime
+    if unit==-6:
+      return [time_diff.microseconds, 'microseonds']
+    elif unit==-3:
+      return [float(time_diff.microseconds)/1000.0, 'milliseconds']
+    else:
+      return [time_diff.seconds,'seconds']
