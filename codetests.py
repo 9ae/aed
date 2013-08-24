fff489e0b259ca41249678a3fd1d54a549a0a2d5
codebunny <alice@valour.me>
2013-08-24 10:17:33 -0400

    implemented time diff module

--- codetests.py
+++ codetests.py
@@ -1,10 +1,13 @@
 import sysaid as sa
 
 def timed_loop_test():
-  T = sa.Toki()
-  for i in range(0,1999):
-    print i
-  b = T.diff(-6)
-  print "loop runtime=%f %s"%(b[0],b[1])
-
-timed_loop_test()
+	T = sa.Toki()
+	sum =0
+	for i in range(0,1999):
+		sum = sum+ i
+	b = T.diffNow('runtime')
+	print "%f \t %f"%(T.get('runtime','milli'),T.get('runtime','micro'))
+  # print "%d :loop runtime=%f %s seconds "%(sum,b,unit)
+	
+for j in range(0,100):
+	timed_loop_test()
--- sysaid.py
+++ sysaid.py
@@ -9,33 +9,51 @@ def find_os():
 		return 2
 	else:
 		return -1
-  
+
 def time_now():
 	import time
 	my_os = find_os()
 	if my_os==0:
-                # gets time in seconds as float
+		# gets time in seconds as float
 		return time.clock()	
 	elif my_os==1 or my_os==2:
 		#gets time in seconds
 		return time.time()
 
 def dt_now():
-        from datetime import datetime
-        dt = datetime.now()
-        return dt.microsecond
+	from datetime import datetime
+	dt = datetime.now()
+	return dt.microsecond
 
 from datetime import datetime
 class Toki:
 
-  def __init__(self):
-    self.startTime = datetime.now()
+	def __init__(self):
+		self.startTime = datetime.now()
+		self.timelog = {}
 
-  def diff(self, unit=1):
-    time_diff = datetime.now() - self.startTime
-    if unit==-6:
-      return [time_diff.microseconds, 'microseonds']
-    elif unit==-3:
-      return [float(time_diff.microseconds)/1000.0, 'milliseconds']
-    else:
-      return [time_diff.seconds,'seconds']
+	def inUnit(self,time_diff,unit):
+		if unit=='micro':
+			return (time_diff.days * 24 * 60 * 60 + time_diff.seconds) * 10e6 + time_diff.microseconds
+		elif unit=='milli':
+			return (time_diff.days * 24 * 60 * 60 + time_diff.seconds) * 1000 + time_diff.microseconds / 1000.0
+		else:
+			return (time_diff.days * 24 * 60 * 60 + time_diff.seconds)
+
+	def diff(self, since, record=None, unit='sec'):
+		time_diff = datetime.now() - since
+		if(record!=None):
+			if isinstance(record, str):
+				self.timelog[record] = time_diff
+			else:
+				print 'record must be a string'
+		return self.inUnit(time_diff,unit)
+
+	def diffNow(self, record=None, unit='sec'):
+		return self.diff(self.startTime, record=record, unit=unit)
+
+	def get(self,recordName,unit=None):
+		if(unit==None):
+			return self.timelog[recordName]
+		else:
+			return self.inUnit(self.timelog[recordName],unit)
